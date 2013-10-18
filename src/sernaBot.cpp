a63636887bc412415047a02603a12a22d918db98
Sergio <sbromberg@usbcali.edu.co>
2013-10-18 02:53:06 -0500

    debug: modos. No calibra...

--- src/sernaBot.cpp
+++ src/sernaBot.cpp
@@ -64,11 +64,11 @@ void SernaBot::update(){
 	
 	// Encontrar el máximo de las necesidades
 	float necesidades [modos.size() - 1];//= new float[  ]; 
-	cout << "TAMANO de necidades" << endl;
+	cout << "TAMANO de necidades  " <<  necesidades.size() << endl;
 	for( int i = 0 ; i < modos.size() ; i++){
 		necesidades[i] = modos.at(i).sensorValue;
+		cout << "necesidad "  << necesidad.mode << "   ... " << necesidad.sensorValue << endl; 
 	}
-	cout <<"NEC  "  << necesidades << endl;
 	
 	
 	// Brujería para encontrar el máximo de las necesidades
