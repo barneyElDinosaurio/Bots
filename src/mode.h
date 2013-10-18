c57b3df6ee18e0bab25c4210411921c49aa37660
Sergio <sbromberg@usbcali.edu.co>
2013-10-18 01:17:04 -0500

    sdfsdf

---
+++ src/mode.h
@@ -0,0 +1,21 @@
+class Mode{
+	public:
+
+	float sensorValue;
+	string mode;
+
+	Mode(){}
+	Mode(string _mode, int _sensorValue){
+		this->mode = _mode;
+		this->sensorValue = _sensorValue;
+	}
+	Mode( string _mode){
+		this->mode = _mode;
+		// Qué hacer con el valor?
+	}
+	void setSensorValue(float theValor){
+		sensorValue = theValor;
+		// ATENCIÓN CON ESTO. ESTO DEBE LLAMARSE AL INICIAR CADA BOT, PORQUE SI NO, TIRA NULL POINTER EXCEPTION.
+	}
+
+};
